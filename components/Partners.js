6e555366d194f5380ed205a66e6aee2ac73abf21
fadlallahjawad <79308991+in2uitions@users.noreply.github.com>
2022-03-09 09:47:44 +0200

    a2
    a2

--- components/Partners.js
+++ components/Partners.js
@@ -12,6 +12,7 @@ export default function Partners() {
       delay: 50,
       disable:"mobile"
     });
+   
     window.addEventListener(
       "touchmove",
       () => {
@@ -19,6 +20,7 @@ export default function Partners() {
       },
       false
     );
+
   });
   return (
     <>
